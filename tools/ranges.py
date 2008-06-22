480366baafa1641ddbed27eebcc9bc17a49cdd59
Steve White <stevan.white@gmail.com>
2008-06-22 09:08:54 +0000

    Brought more into line with OpenType added some ranges fixed bug with ranges outside of font

--- tools/ranges.py
+++ tools/ranges.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/fontforge -script
 """
 Makes an HTML table showing how many glyphs are in each range in each font,
 and tries to collate that with the OS/2 character range support bit flags.
@@ -17,7 +18,7 @@ The intervals are partly just the assigned interval, but often I have
 listed the ranges that have characters assigned to them.
 
 
-$Id: ranges.py,v 1.15 2008-05-18 19:17:24 Stevan_White Exp $
+$Id: ranges.py,v 1.16 2008-06-22 09:08:54 Stevan_White Exp $
 """
 __author__ = "Stevan White <stevan.white@googlemail.com>"
 
@@ -37,26 +38,36 @@ class interval:
 	def __str__( self ):
 		return '[' + str( self.begin ) + ',' + str( self.end ) + ']'
 
+# NOTE the OpenType spec is much more thorough
+# http://www.microsoft.com/OpenType/OTSpec/os2.htm
 ulUnicodeRange = [
 [0,	'Basic Latin', [interval(0x0020, 0x007E)] ],
 [1,	'Latin-1 Supplement',[interval(0x00A0, 0x00FF)] ],
 [2,	'Latin Extended-A',	[interval(0x0100, 0x017F)] ],
 [3,	'Latin Extended-B',     [interval(0x0180, 0x024F)]],
-#Latin Extended C and D?
-[4,	'IPA Extensions',     [interval(0x0250, 0x02AF)]],
-[5,	'Spacing Modifier Letters',     [interval(0x02B0, 0x02FF)]],
+[4,	'IPA and Phoetic Extensions',     [interval(0x0250, 0x02AF),
+			interval(0x1D00, 0x1D7F),	# Phonetic Extensions
+			interval(0x1D80, 0x1DBF)	# Phonetic Extensions S.
+	]],
+[5,	'Spacing Modifier Letters',     [interval(0x02B0, 0x02FF),
+			interval(0xA700, 0xA71F)	# Modifier Tone Letters
+	]],
 [6,	'Combining Diacritical Marks',     [interval(0x0300, 0x036F)]],
-[7,	'Basic Greek',     [interval(0x0370, 0x0377),
+[7,	'Greek and Coptic',     [interval(0x0370, 0x0377),
 			interval(0x037A, 0x037E),
 			interval(0x0384, 0x038A),
 			interval(0x038C, 0x038C),
 			interval(0x038E, 0x03A1),
-			interval(0x03A3, 0x03CF)] ],
-[8,	'Greek Symbols And Coptic',     [interval(0x03D0, 0x03FF)]],	# unclear.
+			interval(0x03A3, 0x03CF),
+			interval(0x03D0, 0x03FF)
+			] ],
+[8,	'Coptic',     [interval(0x2C80, 0x2CFF)]],
 [9,	'Cyrillic',     [
 	interval(0x0400, 0x04FF),	# Cyrillic
-#	interval(0x0500, 0x0523)	# Cyrillic Supplement
-	]	# Cyrillic extended A and B?
+	interval(0x0500, 0x0523),	# Cyrillic Supplement
+	interval(0x2DE0, 0x2DFF),	# Cyrillic Extended-A
+	interval(0xA640, 0xA69F)	# Cyrillic Extended-B
+	]
 	],
 [10,	'Armenian',     [interval(0x0531, 0x0556),
 			interval(0x0559, 0x055F),
@@ -64,23 +75,21 @@ ulUnicodeRange = [
 			interval(0x0589, 0x058A)
 			]
 	],
-[11,	'Basic Hebrew',    [
-			interval(0x05D0, 0x05EA),
-			interval(0x05F0, 0x05F4)
-	]
-	],
-[12,	'Hebrew Extended (A and B blocks combined)',    
-	[interval(0x0591, 0x05C7),
+[11,	'Hebrew',    [
+			interval(0x0591, 0x05C7),
 			interval(0x05D0, 0x05EA),
 			interval(0x05F0, 0x05F4)
 		]], # See http://webcenter.ru/~kazarn/eng/ululinks.htm
-[13,	'Basic Arabic',     [interval(0x0600, 0x0603),
-				interval(0x0606, 0x061B),
-				interval(0x061E, 0x061F),
-				interval(0x0621, 0x0652)
+[12,	'Vai',    [interval(0xA500, 0xA63F),
+		]],
+[13,	'Arabic (+supplement)',     [interval(0x0600, 0x0603),
+			interval(0x0606, 0x061B),
+			interval(0x061E, 0x061F),
+			interval(0x0621, 0x0652),
+			interval(0x0653, 0x06FF),	# Supplement
 	]
 	],
-[14,	'Arabic Extended', [interval(0x0653, 0x06FF)]],	# unclear
+[14,	'', [interval(0x07C0, 0x07FF)]],	# unclear ? Part of Arabic?
 [15,	'Devanagari',     [interval(0x0901, 0x0939),
 			interval(0x093C, 0x094D),
 			interval(0x0950, 0x0954),
@@ -215,10 +224,15 @@ ulUnicodeRange = [
 			]
 		],
 [25,	'Lao',     [interval(0x0E80, 0x0EFF)]],
-[26,	'Basic Georgian',     [interval(0x10D0, 0x10FC)]],	# now these are
-[27,	'Georgian Extended', [interval(0x10A0, 0x10CF)]],	# in same range
+[26,	'Georgian (+supplement)',     [interval(0x10D0, 0x10FC),
+		interval(0x10A0, 0x10CF)]],	# Supplement
+[27,	'Balinese', [interval(0x1B00, 0x1B7F)]],
 [28,	'Hangul Jamo',     [interval(0x1100, 0x11FF)]],
-[29,	'Latin Extended Additional',     [interval(0x1E00, 0x1EFF)]],
+[29,	'Latin Extended (Additional,C,D)',     [
+		interval(0x1E00, 0x1EFF),	# Additional
+		interval(0x2C60, 0x2C7F),	# C
+		interval(0xA720, 0xA72F)	# D
+		]],
 [30,	'Greek Extended',     [interval(0x1F00, 0x1F15),
 		interval(0x1F18, 0x1F1D),
 		interval(0x1F20, 0x1F45),
@@ -236,26 +250,34 @@ ulUnicodeRange = [
 		interval(0x1FF2, 0x1FF4),
 		interval(0x1FF6, 0x1FFE)
 	]],
-[31,	'General Punctuation',     [interval(0x2000, 0x2060)]],
-[32,	'Superscripts And Subscripts',     [interval(0x2070, 0x2071),
+[31,	'General Punctuation (+suppl)',     [interval(0x2000, 0x2060),
+		interval(0x2E00, 0x2E7F),	# Supplemental
+	]],
+[32,	'Superscripts and Subscripts',     [interval(0x2070, 0x2071),
 		interval(0x2074, 0x208E),
 		interval(0x2080, 0x2084)
 	]
 	],
 [33,	'Currency Symbols',     [interval(0x20A0, 0x20B5)]],
-[34,	'Combining Diacritical Marks For Symbols',     [interval(0x20D0, 0x20F0)]],
+[34,	'Combining Diacritical Marks for Symbols',     [interval(0x20D0, 0x20F0)]],
 [35,	'Letterlike Symbols',     [interval(0x2100, 0x214F)]],
 [36,	'Number Forms',     [interval(0x2153, 0x2188)]],
-[37,	'Arrows',     [interval(0x2190, 0x21FF)]],
-[38,	'Mathematical Operators',     [ #FIXME there are subranges
+[37,	'Arrows (+suppl)',     [interval(0x2190, 0x21FF),
+	interval(0x27F0, 0x27FF),	# Supplemental Arrows-A
+	interval(0x2900, 0x297F),	# Supplemental Arrows-B
+	interval(0x2B00, 0x2BFF)	# Miscellaneous Symbols and Arrows
+	]],
+[38,	'Mathematical Operators',     [ 
 	interval(0x2200, 0x22FF),
 	interval(0x2A00, 0x2AFF),	# Supplemental Mathematical Operators
-	interval(0x27C0, 0x27EF),	# Miscellaneous Mathematical Symbols-A
+	interval(0x27C0, 0x27CA),	# Miscellaneous Mathematical Symbols-A
+	interval(0x27CC, 0x27CC),
+	interval(0x27D0, 0x27EF),
 	interval(0x2980, 0x29FF)	# Miscellaneous Mathematical Symbols-B
 	]
 		],
-[39,	'Miscellaneous Technical',     [interval(0x2300, 0x23FF)]],
-[40,	'Control Pictures',     [interval(0x2400, 0x243F)]],
+[39,	'Miscellaneous Technical',     [interval(0x2300, 0x23E7)]],
+[40,	'Control Pictures',     [interval(0x2400, 0x2426)]],
 [41,	'Optical Character Recognition',     [interval(0x2440, 0x245F)]],
 [42,	'Enclosed Alphanumerics',     [interval(0x2460, 0x24FF)]],
 [43,	'Box Drawing',     [interval(0x2500, 0x257F)]],
@@ -266,19 +288,30 @@ ulUnicodeRange = [
 			interval(0x26A0, 0x26C3)
 			]
 			],
-[47,	'Dingbats',     [interval(0x2701, 0x27BF)]],	#FIXME several intervals
-[48,	'CJK Symbols And Punctuation', [interval(0x3000, 0x303F)]],
+[47,	'Dingbats',     [interval(0x2701, 0x2704),
+			interval(0x2706, 0x2709),
+			interval(0x270C, 0x2727),
+			interval(0x2729, 0x274B),
+			interval(0x274D, 0x274D),
+			interval(0x274F, 0x2752),
+			interval(0x2756, 0x2756),
+			interval(0x2758, 0x275E),
+			interval(0x2761, 0x2794),
+			interval(0x2798, 0x27AF),
+			interval(0x27B1, 0x27BE)
+	]],
+[48,	'CJK Symbols and Punctuation', [interval(0x3000, 0x303F)]],
 [49,	'Hiragana', [interval(0x3040, 0x309F)]],
 [50,	'Katakana', [interval(0x30A0, 0x30FF)]],
 [51,	'Bopomofo', [interval(0x3100, 0x312F)]],
 [52,	'Hangul Compatibility Jamo', [interval(0x3130, 0x318F)]],
 [53,	'CJK Miscellaneous', [interval(0x3190, 0x319F)]],
-[54,	'Enclosed CJK Letters And Months', [interval(0x3200, 0x32FF)]],
+[54,	'Enclosed CJK Letters and Months', [interval(0x3200, 0x32FF)]],
 [55,	'CJK Compatibility', [interval(0x3300, 0x33FF)]],
 [56,	'Hangul', [interval(0x3400, 0x3D2D)]],
-[57,	'Hangul Supplementary-A', [interval(0x3D2E, 0x44B7)]],
-[58,	'Hangul Supplementary-B', [interval(0x44B8, 0x4DFF)]],
-[59,	'CJK Unified Ideographs', [interval(0x4E00, 0x9FFF)]],
+[57,	'Non-Plane 0', [interval(0xD800, 0xDFFF)]],
+[58,	'Phoenician', [interval(0x10900, 0x1091F)], True],
+[59,	'CJK Unified Ideographs', [interval(0x4E00, 0x9FFF)]], #FIXME complex
 [60,	'Private Use Area', [interval(0xE800, 0xF8FF)]],
 [61,	'CJK Compatibility Ideographs', [interval(0xF900, 0xFAFF)]],
 [62,	'Alphabetic Presentation Forms', [
@@ -299,7 +332,9 @@ ulUnicodeRange = [
 				]
 		],
 [64,	'Combining Half Marks', [interval(0xFE20, 0xFE2F)]],
-[65,	'CJK Compatibility Forms', [interval(0xFE30, 0xFE4F)]],
+[65,	'CJK Compatibility Forms', [interval(0xFE10, 0xFE1F),	# Vertical forms
+		interval(0xFE30, 0xFE4F)	# Compatability forms
+	]],
 [66,	'Small Form Variants', [interval(0xFE50, 0xFE52),
 				interval(0xFE54, 0xFE66),
 				interval(0xFE58, 0xFE5B)
@@ -310,8 +345,8 @@ ulUnicodeRange = [
 				interval(0xFEFF, 0xFEFF)
 				]
 		],
-[68,	'Halfwidth And Fullwidth Forms', [interval(0xFF00, 0xFFEF)]],
-[69,	'Specials', [interval(0xFFF0, 0xFFFD)]],
+[68,	'Halfwidth and Fullwidth Forms', [interval(0xFF00, 0xFFEF)]],
+[69,	'Specials', [interval(0xFFF9, 0xFFFD)]],
 [70, 	'Tibetan', [interval(0x0F00, 0x0FFF)]],
 [71, 	'Syriac', [interval(0x0700, 0x070D),
 		interval(0x070F, 0x074A),
@@ -320,7 +355,7 @@ ulUnicodeRange = [
 [72, 	'Thaana', [interval(0x0780, 0x07B1)]],
 [73, 	'Sinhala', [interval(0x0D80, 0x0DFF)]],
 [74, 	'Myanmar', [interval(0x1000, 0x109F)]],
-[75, 	'Ethiopic', [
+[75, 	'Ethiopic (+supplement, extended)', [
 		interval(0x1200, 0x1248),
 		interval(0x124A, 0x124D),
 		interval(0x1250, 0x1256),
@@ -330,14 +365,18 @@ ulUnicodeRange = [
 		interval(0x128A, 0x128D),
 		interval(0x1290, 0x12B0),
 		interval(0x12B2, 0x12B5),
-		interval(0x12B8, 0x12BE)
+		interval(0x12B8, 0x12BE),
+		interval(0x1380, 0x139F),	# supplement
+		interval(0x2D80, 0x2DDF)	# extended
 		]
 		],
 [76,	'Cherokee', [interval(0x13A0, 0x13F4)]],
 [77, 	'Unified Canadian Aboriginal Syllabics', [interval(0x1401, 0x14DF)]],
 [78, 	'Ogham', [interval(0x1680, 0x169F)]],
 [79, 	'Runic', [interval(0x16A0, 0x16F1)]],
-[80, 	'Khmer', [interval(0x1780, 0x17FF)]],
+[80, 	'Khmer (+symbols)', [interval(0x1780, 0x17FF),
+		interval(0x19E0, 0x19FF)	# symbols
+	]],
 [81, 	'Mongolian', [interval(0x1800, 0x18AF)]],	#FIXME ranges
 [82, 	'Braille Patterns', [interval(0x2800, 0x28FF)]],
 [83, 	'Yi Syllables, Radicals', [interval(0xA000, 0xA0EF),
@@ -350,18 +389,63 @@ ulUnicodeRange = [
 		interval(0x1750, 0x1773)
 		]
 		],
-[85, 	'Old Italic', [interval(0x10300, 0x10320)]],
-[86, 	'Gothic', [interval(0x10330, 0x1034A)]],
-[87, 	'Deseret', [interval(0x10400, 0x1044F)]],
-[88, 	'Byzantine & Western Musical Symbols', [interval(0x1D000, 0x1D0F5),
-			interval(0x1D100, 0x1D1DD)]],
-[89, 	'Mathematical Alphanumeric Symbols', [interval(0x1D400, 0x1D4FF)]],
-[90, 	'Private Use (plane 15,16)', []],
-[91, 	'Variation Selectors', []],
-[92, 	'Tags', []],
-[93, 	'Reserved for Unicode SubRanges', []],
-[94, 	'Reserved for Unicode SubRanges', []],
-[95, 	'Reserved for Unicode SubRanges', []],
+[85, 	'Old Italic', [interval(0x10300, 0x10320)], True],
+[86, 	'Gothic', [interval(0x10330, 0x1034A)], True],
+[87, 	'Deseret', [interval(0x10400, 0x1044F)], True],
+[88, 	'Byzantine &amp; Western Musical Symbols', [interval(0x1D000, 0x1D0F5),
+			interval(0x1D100, 0x1D1DD),
+			interval(0x1D200, 0x1D24F)
+			], True],
+[89, 	'Mathematical Alphanumeric Symbols', [interval(0x1D400, 0x1D4FF)], True],
+[90, 	'Private Use (plane 15,16)', [
+		interval(0xFF000, 0xFFFFD),	# plane 15
+		interval(0x100000, 0x10FFFD)	# plane 16
+	], True],
+[91, 	'Variation Selectors (+suppl)', [interval(0xFE00, 0xFE0F),
+		interval(0xE0100, 0xE01EF)	# supplement
+		], True],
+[92, 	'Tags', [interval(0xE0000, 0xE01EF)], True],
+[93, 	'Limbu', [interval(0x1900, 0x194F)]],
+[94, 	'Tai Le', [interval(0x1950, 0x196D),
+		interval(0x1970, 0x1974)
+	]],
+[95, 	'New Tai Lue', [interval(0x1980, 0x19DF)]],
+[96, 	'Buginese', [interval(0x1A00, 0x1A1F)]],
+[97, 	'Glagolitic', [interval(0x2C00, 0x2C5F)]],
+[98, 	'Tifinagh', [interval(0x2D30, 0x2D7F)]],
+[99, 	'Ying Hexagram Symbols', [interval(0x4DC0, 0x4DFF)]],
+[100, 	'Syloti Nagri', [interval(0xA800, 0xA82F)]],
+[101, 	'Linear B Syllabary etc', [interval(0x10000, 0x1013F)], True],
+[102, 	'Ancient Greek Numbers', [interval(0x10140, 0x1018F)], True],
+[103, 	'Ugaritic', [interval(0x10380, 0x1039D),
+		interval(0x1039F, 0x1039F)
+	], True],
+[104, 	'Old Persian', [interval(0x103A0, 0x103C3),
+		interval(0x103C8, 0x103D6),
+	], True],
+[105, 	'Shavian', [interval(0x10450, 0x1047F)], True],
+[106, 	'Osmanya', [interval(0x10480, 0x104AF)], True],
+[107, 	'Cypriot Syllabary', [interval(0x10800, 0x1083F)], True],
+[108, 	'Kharoshthi', [interval(0x10A00, 0x10A5F)], True],
+[109, 	'Tai Xuan Jing Symbols', [interval(0x1D300, 0x1D35F)], True],
+[110, 	'Cuneiform (+numbers)', [interval(0x12000, 0x1247F)], True],
+[111, 	'Counting Rod Numerals', [interval(0x1D360, 0x1D37F)], True],
+[112, 	'Sundanese', [interval(0x1B80, 0x1BBF)]],
+[113, 	'Lepcha', [interval(0x1C00, 0x1C4F)]],
+[114, 	'Ol Chiki', [interval(0x1C50, 0x1C7F)]],
+[115, 	'Saurashtra', [interval(0xA880, 0xA8DF)]],
+[116, 	'Kayah Li', [interval(0xA900, 0xA92F)]],
+[117, 	'Reiang', [interval(0xA930, 0xA95F)]],
+[118, 	'Cham', [interval(0xAA00, 0xAA5F)]],
+[119, 	'Ancient Symbols', [interval(0x10190, 0x101CF)], True],
+[120, 	'Phaistos Disc', [interval(0x101D0, 0x101FF)], True],
+[121, 	'Carian, Lycian, Lydian', [interval(0x102A0, 0x102DF),
+		interval(0x10280, 0x1029F),      # Lycian
+		interval(0x10920, 0x1093F)      # Lydian
+	], True],
+[122, 	'Domino and Mahjong Tiles', [interval(0x1F030, 0x1F09F),	# Domino
+		interval(0x1F000, 0x1F02F)      # Mahjong
+	], True],
 #[96-127, 	'Reserved for Unicode SubRanges', []]
 ]
 
@@ -564,17 +648,17 @@ def count_glyphs_in_intervals( font, intervals ):
 	for r in intervals:
 		# select() will throw up if try to select value 
 		# beyond the range of the encoding
-		if r.begin < len( font ) and r.begin < len( font ):
-			font.selection.select( ( 'ranges', None ),
+		if r.begin < len( font ) and r.end < len( font ):
+			try: 
+				font.selection.select( ( 'ranges', None ),
 					r.begin, r.end )
-		g = font.selection.byGlyphs
-		for e in g:
-			num += 1
+				g = font.selection.byGlyphs
+				for e in g:
+					num += 1
+			except ValueError:
+				print >> sys.stderr, "interval " + str( r ) \
+				+ " not representable in " + font.fontname
 	return num
-
-def intervals_to_stderr( intervals ):
-	for r in intervals:
-		print >> sys.stderr, "\t" + str( r )
 
 def collect_range_info( fontSupport, font, os2supportbit, bit, offset ):
 	supports = ( os2supportbit & (1 << bit) ) != 0
@@ -582,12 +666,7 @@ def collect_range_info( fontSupport, font, os2supportbit, bit, offset ):
 	rangeName = ulUnicodeRange[index][1]
 	intervals = ulUnicodeRange[index][2]
 	nglyphs = 0
-	try: 
-		nglyphs = count_glyphs_in_intervals( font, intervals )
-	except ValueError:
-		print >> sys.stderr, "Problem with interval in " + font.fontname
-		print >> sys.stderr, "\t" + rangeName
-		intervals_to_stderr( intervals )
+	nglyphs = count_glyphs_in_intervals( font, intervals )
 	fontSupport.setRangeSupport( bit, supports, nglyphs )
 
 class SupportInfo:
@@ -620,17 +699,22 @@ def collect_font_range_report( fontPath, short ):
 
 	r = font.os2_unicoderanges
 
+#	print >> sys.stderr, hex( r[0] ), hex( r[1] ),hex( r[2] ),hex( r[3] );
+
 	fontSupport = FontSupport( font.fontname, short )
 	fontSupportList.append( fontSupport )
 
-	for bit in xrange(0,32):
+	for bit in range(0,32):
 		collect_range_info( fontSupport, font, r[0], bit, 0 )
 
-	for bit in xrange(0,32):
+	for bit in range(0,32):
 		collect_range_info( fontSupport, font, r[1], bit, 32 )
 
-	for bit in xrange(0,32):
+	for bit in range(0,32):
 		collect_range_info( fontSupport, font, r[2], bit, 64 )
+
+	for bit in range(0,27):
+		collect_range_info( fontSupport, font, r[3], bit, 96 )
 
 def print_font_range_table():
 	print '<table class="fontrangereport" frame="box" rules="all">'
@@ -649,7 +733,12 @@ def print_font_range_table():
 		bit = r[0]
 		range_name = r[1]
 		intervals = r[2]
-		print '<tr><td>' + range_name + '</td>' 
+
+		rowclass = ' class="low"'
+		if len( ulUnicodeRange[bit] ) > 3 and ulUnicodeRange[ bit ][3]:
+			rowclass = ' class="high"'
+			
+		print '<tr' + rowclass + '><td>' + range_name + '</td>' 
 		print '<td class="num">' + str( total_intervals( intervals ) ) \
 			+ '</td>'
 		print '<td></td>' 
@@ -692,6 +781,9 @@ defined in the TrueType standard.
 Note the 2 characters from Devanagri.  These are the danda and double-danda
 used by other Inidic scripts.
 </p>
+<p>
+The ranges <span style="color: gray">beyond Unicode 0xFFFF</span>, are shaded.
+</p>
 """
 
 def print_font_range_report():
@@ -701,6 +793,7 @@ def print_font_range_report():
 	print 'Gnu FreeFont character range support'
 	print '</title>'
 	print '<style type="text/css">'
+	print '	tr.high { color: gray }'
 	print '	td.num { text-align: right }'
 	print '	th { padding: 0.5em }'
 	print '	caption { font-size: larger; font-weight: bold; }'
