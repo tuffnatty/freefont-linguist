dbbafe659b15c8405d0d01ea44433165fd4a4786
Steve White <stevan.white@gmail.com>
2013-09-01 12:09:04 +0000

    Utilities for naming glyphs

--- tools/script-menu/nameByNamelist.py
+++ tools/script-menu/nameByNamelist.py
@@ -15,7 +15,7 @@ GNU FreeFont.  If not, see <http://www.gnu.org/licenses/>.
 """
 __author__ = "Stevan White"
 __email__ = "stevan.white@googlemail.com"
-__copyright__ = "Copyright 2009, 2010, Stevan White"
+__copyright__ = "Copyright 2013, Stevan White"
 __date__ = "$Date:: 2013-04-09 12:44:41 +0200#$"
 __version__ = "$Revision: 1.3 $"
 
@@ -23,18 +23,34 @@ __doc__ = """
 For use from the FontForge Script Menu.
 Add it to the Scripts Menu using the Preferences dialog.
 
-Sets the name and unicode values of the selected range of slots to the
-encoding, that is
-	Name:	uniXXXX
-	Unicode:	u+XXXX
-where XXXX is the n-digit hex value for the slot encoding.
+User must provide the full path to a FontForge Namelist file (as produced
+by Encodint -> Save Namelist of Font) in the variable nameListFilePath.
 
-Careful! it changes the value whether it was previously set or not.
+Sets the name and unicode values of the selected range of slots to the
+encoding equalt to the slot number, and a name looked up in a Namelist
+file provided by the user.
+
+If the encoding of a selected glyph is not represented in the file, or
+if glyph at the slot has the same name as indicated by the file, nothing
+is changed.
+
+Careful! it changes values whether they were previously set or not.
 
 Detailed info is printed to standard output (see by launching FontForge
 from a console).
 """
 import fontforge
+
+nameListFilePath = '/home/swhite/.FontForge/FreeSerif.nam'
+
+def readNameList( filename ):
+	f = open( filename )
+	slotName = {}
+	for line in f:
+		( slot, name ) = line.split()
+		slotval = int( slot, 16 )
+		slotName[slotval] = name
+	return slotName
 
 def explain_error_and_quit( e ):
 	if e:
@@ -42,21 +58,24 @@ def explain_error_and_quit( e ):
 	exit( 1 )
 
 try:
+	print( 'Re-naming glyphs according to name list ' + nameListFilePath )
+	slotName = readNameList( nameListFilePath )
 	glyphs = fontforge.activeFont().selection.byGlyphs
 	for g in glyphs:
-		if g.encoding <= 0xFFFF:
-			newname = 'uni%0.4X' %( g.encoding )
-		elif g.encoding <= 0xFFFFF:
-			newname = 'uni%0.5X' %( g.encoding )
-		elif g.encoding <= 0xFFFFFF:
-			newname = 'uni%0.6X' %( g.encoding )
-		elif g.encoding <= 0xFFFFFFF:
-			newname = 'uni%0.7X' %( g.encoding )
-		elif g.encoding <= 0xFFFFFFFF:
-			newname = 'uni%0.8X' %( g.encoding )
-		print "naming " + str( g.glyphname ) + ' as ' + newname
-		g.glyphname = newname
-		g.unicode = g.encoding
+		if g.encoding in slotName:
+			newname = slotName[g.encoding]
+			if g.glyphname == newname:
+				print( "glyph at " + hex( g.encoding )
+					+ ' already named ' + newname )
+			else:
+
+				print( "naming " + str( g.glyphname )
+					+ ' as ' + newname )
+				g.glyphname = newname
+				g.unicode = g.encoding
+		else:
+			print ( "slot value " + hex( g.encoding )
+				+ ' not found in name list')
 except ValueError, e:
 	explain_error_and_quit( e )
 
